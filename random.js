1abfc0693313a4aeb8c994a78c0f671e01dc5c26
keertikumari12 <115786541+keertikumari12@users.noreply.github.com>
2023-10-09 13:51:30 +0530

    Add files via upload

---
+++ random.js
@@ -0,0 +1,16 @@
+let randomNum=Math.random();
+console.log(randomNum);
+
+let floor=Math.floor(randomNum*10);
+let ceil=Math.ceil(randomNum*10);
+console.log(floor);
+console.log(ceil);
+
+let cuberoot=Math.cbrt(64);
+console.log(cuberoot);
+
+let squareroot=Math.sqrt(64);
+console.log(squareroot)
+
+let pi=Math.PI;
+console.log(pi);
